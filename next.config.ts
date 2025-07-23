e57ffab614d6c39bfbadbb57f05d892e9e4d06fe
Swathi Surendran <swathisuren247@gmail.com>
2025-07-23 14:57:25 +0400

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "100MB",
+    },
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "static.vecteezy.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cloud.appwrite.io",
+      },
+      {
+        protocol: "https",
+        hostname: "fra.cloud.appwrite.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
