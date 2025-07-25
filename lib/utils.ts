6f7c79dae0298af1fcf22eb9ef92bd8b9b45de26
Swathi Surendran <swathisuren247@gmail.com>
2025-07-25 22:44:00 +0400

    fixed

--- lib/utils.ts
+++ lib/utils.ts
@@ -181,10 +181,20 @@ export const constructFileUrl = (bucketFileId: string) => {
 export const constructDownloadUrl = (bucketFileId: string) => {
   return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/download?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`;
 };
+export type CategoryUsage = {
+  size: number;
+  latestDate: string;
+};
 
+export type TotalSpace = {
+  document: CategoryUsage;
+  image: CategoryUsage;
+  video: CategoryUsage;
+  audio: CategoryUsage;
+  other: CategoryUsage;
+};
 // DASHBOARD UTILS
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const getUsageSummary = (totalSpace: any) => {
+export const getUsageSummary = (totalSpace: TotalSpace) => {
   return [
     {
       title: "Documents",
