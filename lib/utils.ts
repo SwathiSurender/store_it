d0551fad5229a05a3fda37613a1410c7d386d159
Swathi Surendran <swathisuren247@gmail.com>
2025-07-28 11:19:10 +0400

    issue fixed

--- lib/utils.ts
+++ lib/utils.ts
@@ -175,26 +175,15 @@ export const getFileIcon = (
 // APPWRITE URL UTILS
 // Construct appwrite file URL - https://appwrite.io/docs/apis/rest#images
 export const constructFileUrl = (bucketFileId: string) => {
-  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`;
+  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/view?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
 };
 
 export const constructDownloadUrl = (bucketFileId: string) => {
-  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/download?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID}`;
-};
-export type CategoryUsage = {
-  size: number;
-  latestDate: string;
+  return `${process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT}/storage/buckets/${process.env.NEXT_PUBLIC_APPWRITE_BUCKET}/files/${bucketFileId}/download?project=${process.env.NEXT_PUBLIC_APPWRITE_PROJECT}`;
 };
 
-export type TotalSpace = {
-  document: CategoryUsage;
-  image: CategoryUsage;
-  video: CategoryUsage;
-  audio: CategoryUsage;
-  other: CategoryUsage;
-};
 // DASHBOARD UTILS
-export const getUsageSummary = (totalSpace: TotalSpace) => {
+export const getUsageSummary = (totalSpace: any) => {
   return [
     {
       title: "Documents",
